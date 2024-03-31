24da8f434ac8d92ddb12b480a9b1f6765dc0019a
MissWhale <doden5180@gmail.com>
2024-03-31 19:28:07 +0900

    feat: input validate

--- package/App.vue
+++ package/App.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-import "@/assets/base.css";
-import "@/assets/main.scss";
-import MIcon from "@/components/MIcon/MIcon.vue";
-import MInput from "@/components/MInput/MInput.vue";
-import { ref } from "vue";
-const test = ref();
+import '@/assets/base.css'
+import '@/assets/main.scss'
+import MIcon from '@/components/MIcon/MIcon.vue'
+import MInput from '@/components/MInput/MInput.vue'
+import { ref } from 'vue'
+const validate = ref(false)
+const rule = [(value: any) => !!value || 'test']
+const test = ref()
+const input = ref()
+const reset = () => {
+  console.log(input.value)
+}
 </script>
 
 <template>
   <!-- <Icon icon="mdi:circle" color="primary"></Icon> -->
   <div class="test">
-    <MInput v-model="test" color="primary" clearable></MInput>
-    <MInput v-model="test" color="primary" clearable></MInput>
-    <MInput v-model="test" color="primary" clearable></MInput>
+    <MInput
+      ref="input"
+      v-model="test"
+      v-model:validate="validate"
+      :rules="rule"
+      color="primary"
+      clearable
+    ></MInput>
+    <!-- <MInput v-model="test" color="primary" clearable></MInput>
+    <MInput v-model="test" color="primary" clearable></MInput> -->
+    <span @click="reset">{{ validate }}</span>
   </div>
 </template>
 
